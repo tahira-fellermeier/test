3ef74580de198dcb265bc94bf6dd8c282ffbaf99
Tahira Fellermeier <fellermeiertahira@gmail.com>
2024-11-04 10:22:26 +0100

    Test commit

--- src/main/java/edu/fra/uas/controller/HelloWorldController.java
+++ src/main/java/edu/fra/uas/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//Kommentar
 
 @Controller
 public class HelloWorldController {
